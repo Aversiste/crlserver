691cce56073c78e89e11fd49d742b3c0cb830a3a
Aversiste <aversiste@gmail.com>
2011-07-22 16:11:24 +0200

    Remove the trailing 'r' in the licence header

--- init.c
+++ init.c
@@ -1,7 +1,6 @@
 /*
  * Copyright (c) 2011 Tristan Le Guern <leguern@medu.se>
  *
- r
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
