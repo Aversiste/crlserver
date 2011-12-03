569f642eee674fbcb0e5d615f73c23f9d3efeb37
Aversiste <aversiste@gmail.com>
2011-12-03 21:46:33 +0100

    Remove a comment about uncertain futur

--- extern.c
+++ extern.c
@@ -24,13 +24,3 @@ struct session session;
 games_list_head glh = SLIST_HEAD_INITIALIZER(glh);
 editors_list_head elh = SLIST_HEAD_INITIALIZER(elh);
 
-/* Futur configuration option variables for the server: */
-/*
-char  *conf_servername;
-char  *conf_banner;
-int    conf_maxusers = 64000;
-int    conf_maxnamelen = 20;
-int    conf_editors;
-int    conf_games;
-*/
-
